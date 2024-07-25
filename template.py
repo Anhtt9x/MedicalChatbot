1ac9835c9290288370d0640abd3a21ac502c0af9
Anhtt9x <anhtt454598@gmail.com>
2024-07-25 10:03:44 +0700

    structured

---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s : ")
+
+list_of_files = [
+    
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "research/trials.ipynb",
+    "app.py",
+    "store_index.py",
+    "static/.gitkeep",
+    "templates/chat.html"
+
+]
+
+for file in list_of_files:
+    file = Path(file)
+    file_dir , file_name = os.path.split(file)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info("Making dir {file_dir} for {file_name}")
+    
+    if (not os.path.exists(file)) or (os.path.getsize(file)==0):
+        with open(file, 'w') as f:
+            pass
+            logging.info("Making file left")
+
+    else:
+        logging.info("File {file} already excist")
